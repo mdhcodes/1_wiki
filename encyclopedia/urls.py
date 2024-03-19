2adc947eafa2f33ba12977e0eeef3ee4212c412d
mdhcodes <mdhcodes@gmail.com>
2024-03-19 13:38:28 -0400

    Add page.html and none.html

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    # Entry Page requirements must include a url path to each entry/page title (CSS, DJANGO, GIT, HTML, PYTHON, etc.) here.
+    path("<str:name>", views.page, name="page")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -8,3 +8,15 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
+# Entry Page requirements must render page.html for each entry/page title (CSS, DJANGO, GIT, HTML, PYTHON, etc.) here.
+def page(request, name):
+    title = name
+    entry = util.get_entry(title)
+    if entry is None:
+        return render(request, "encyclopedia/none.html")
+    else:
+        return render(request, "encyclopedia/page.html", {
+            "title": title,
+            "entry": entry
+        })
